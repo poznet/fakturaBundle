b46dc67bc88fa1ba3c195f29bd0c8f381321c64f
Michał Glajc <pozycjoner.net@gmail.com>
2019-02-27 23:25:38 +0100

    sorotwanie po firmie

--- src/poznet/FakturaBundle/Repository/FakturaRepository.php
+++ src/poznet/FakturaBundle/Repository/FakturaRepository.php
@@ -11,25 +11,42 @@ namespace poznet\FakturaBundle\Repository;
 class FakturaRepository extends \Doctrine\ORM\EntityRepository
 {
 
-    public function findLastNumberForMonth(\DateTime $data)
+    public function findLastNumberForMonth(\DateTime $data, $firm = null)
     {
-        $result = $this->getEntityManager()->createQuery('
+        if($firm==0) $firm=null;
+        if ($firm != null) {
+            $result = $this->getEntityManager()->createQuery('
+        SELECT F from FakturaBundle:Faktura F
+        WHERE MONTH(F.data_wystawienie)=:miesiac
+        AND WHERE F.sprzedawcaId=:firm
+        ORDER BY F.id DESC  ')
+                ->setParameter('firm', $firm);
+        } else {
+            $result = $this->getEntityManager()->createQuery('
         SELECT F from FakturaBundle:Faktura F
         WHERE MONTH(F.data_wystawienie)=:miesiac
         ORDER BY F.id DESC 
-        ')
+        ');
+        }
+        $result
             ->setParameter('miesiac', $data->format('m'))
             ->getFirstResult();
 
         return $result;
     }
 
-    public function findByMiesiac($rok, $miesiac)
+    public function findByMiesiac($rok, $miesiac, $firm = null)
     {
+        if($firm==0) $firm=null;
         $qb = $this->createQueryBuilder('f')
-            ->andWhere('f.dataWystawienia  LIKE :forDate')
-            ->setParameter('forDate', $rok . '-' . $miesiac . '-%')
-            ->getQuery();
-        return $qb->execute();
+            ->andWhere('f.dataWystawienia  LIKE :forDate');
+        if ($firm != null) {
+            $qb->andWhere('f.sprzedawcaId  LIKE :firm')
+                ->setParameter('firm', $firm);
+        }
+        $qb
+            ->setParameter('forDate', $rok . '-' . $miesiac . '-%');
+
+        return $qb->getQuery()->execute();
     }
 }
